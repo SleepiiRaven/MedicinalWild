0ef08b116c296de3f52f34541fb3b423085ec4f7
Damon <pvdoriginal2019@gmail.com>
2022-04-26 18:41:45 +0300

    Added friction to the player

--- Medicinal Wild/Assets/Player.cs
+++ Medicinal Wild/Assets/Player.cs
@@ -9,37 +9,92 @@ public class Player : MonoBehaviour {
 
     // these are also variables i think
     private BoxCollider2D boxCollider;
+    private Rigidbody2D rig;
 
     // these are variables i think
     private Vector3 moveDelta;
 
     private SpriteRenderer spriteRenderer;
+     
+    [SerializeField] private float maxSpeed = 10; // max speed the character can reach
+    private float speed = 0; // the current speed
+
+    [SerializeField] private float acceleration = 1; // how fast the character reaches  max speed
+
+    private bool isMoving = false;
 
     private void Start() {
 
         boxCollider = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rig = GetComponent<Rigidbody2D>();
+
+        // starts running the UpdateSpeed coroutine
+        StartCoroutine(UpdateSpeed());
     }
 
+
     // don't use this a lot for keybinds
-    private void FixedUpdate() {
+    private void Update() {
         // make sure you have a vector that keeps track of the delta movement
         // Look for inputs
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        // Reset moveDelta
-        moveDelta = new Vector3(x,y,0);
-
-        // swap sprite direction, wether going right or left
-        if (moveDelta.x > 0)
+        //flips the character to the direction they're moving
+        if (x > 0)
             spriteRenderer.flipX = false;
-        else if (moveDelta.x < 0)
+        else if (x < 0)
             spriteRenderer.flipX = true;
 
-        // make it move
-        // make speed equal
-        
-        transform.Translate(moveDelta * Time.deltaTime);
+        if (x != 0 || y != 0) // if character's moving
+        {
+            moveDelta = new Vector3(x, y);
+            isMoving = true;
+        }
+        else // if character's not moving
+        {
+            isMoving = false;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {   
+        // moving the character using physics, so it can properly collide with objects
+        rig.MovePosition(Vector2.MoveTowards(transform.position, transform.position + moveDelta*10, speed * Time.fixedDeltaTime));
+        //                                    current position       target position                        speed
+    }
+
+    private IEnumerator UpdateSpeed()
+    {
+        while(true) // infinite loop
+        {   
+            // each frame it checks wheter the character's moving or not, and 
+            // accelerates or slows down accordingly
+
+            if(isMoving)
+            {
+                //speed needs to increase towards max speed
+
+                if (speed < maxSpeed)
+                    speed += acceleration * Time.deltaTime;
+
+                else if (speed > maxSpeed)
+                    speed = maxSpeed;
+            }
+            if(!isMoving)
+            {
+                //speed needs to decrease towards 0
+
+                if (speed > 0)
+                    speed -= acceleration * Time.deltaTime;
+
+                else if (speed < 0)
+                    speed = 0;
+            }
+
+            yield return new WaitForEndOfFrame(); // waits for end of frame to continue, like the update function
+        }
     }
 }
