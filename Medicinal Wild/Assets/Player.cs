cfc8f8be79ab824253a362d61299e4044e820851
Damon <pvdoriginal2019@gmail.com>
2022-04-26 18:19:20 +0300

    improves flip mechanic

--- Medicinal Wild/Assets/Player.cs
+++ Medicinal Wild/Assets/Player.cs
@@ -13,8 +13,12 @@ public class Player : MonoBehaviour {
     // these are variables i think
     private Vector3 moveDelta;
 
+    private SpriteRenderer spriteRenderer;
+
     private void Start() {
+
         boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // don't use this a lot for keybinds
@@ -27,19 +31,15 @@ public class Player : MonoBehaviour {
         // Reset moveDelta
         moveDelta = new Vector3(x,y,0);
 
-       
-
-
         // swap sprite direction, wether going right or left
         if (moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if (moveDelta.x < 0) 
-            transform.localScale = new Vector3(-1, 1, 1);
+            spriteRenderer.flipX = false;
+        else if (moveDelta.x < 0)
+            spriteRenderer.flipX = true;
 
         // make it move
         // make speed equal
         
         transform.Translate(moveDelta * Time.deltaTime);
-        
     }
 }
