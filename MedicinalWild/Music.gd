f49183f7f2e9b45b06af3f7005a107386754175f
SleepiiRaven <sleepingravenytb@gmail.com>
2022-05-16 16:53:53 -0700

    Update Music.gd

--- MedicinalWild/Music.gd
+++ MedicinalWild/Music.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 signal fade_started
 
-#music is annoying to do :( AAAAAAAAAAAAAAA this is dumb
+#music is annoying to do :( AAAAAAAAAAAAAAA this is dumb please help
 
 #TRANSITION FOR MUSIC
 export var TRANSITION_DURATION = 4.0
